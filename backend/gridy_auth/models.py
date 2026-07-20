eed46cef7b74e3bb223a292bd20fbbc8be54dc0a
ur1el0 <zanoroosc@gmail.com>
2026-07-20 12:06:05 +0800

    feat(auth): add Resident model with 1-1 relation to User

--- backend/gridy_auth/models.py
+++ backend/gridy_auth/models.py
@@ -1,4 +1,3 @@
-from django.utils import choices
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -7,8 +6,8 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     class Role(models.TextChoices):
-        ADMIN = 'ADMIN', 'Admin',
-        RESIDENT = 'RESIDENT', 'Resident',
+        ADMIN = 'ADMIN', 'Admin'
+        RESIDENT = 'RESIDENT', 'Resident'
 
     role = models.CharField(
         max_length=50,
@@ -16,3 +15,13 @@ class User(AbstractUser):
         default=Role.RESIDENT,
     )
         
+class Resident(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    full_name = models.CharField(max_length=255)
+    birth_date = models.DateField()
+    voter_status = models.BooleanField(default=False)
+    contact_number = models.CharField(max_length=20, null=True, blank=True)
+    
+
+    def __str__(self):
+        return f'{self.full_name}'
