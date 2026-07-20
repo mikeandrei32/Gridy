65509816c3401a9c30b1e37f315f2a37707a8d9d
ur1el0 <zanoroosc@gmail.com>
2026-07-21 01:53:21 +0800

    feat(auth): configure modular api url routing under /api/v1/auth/

--- backend/gridy_auth/views.py
+++ backend/gridy_auth/views.py
@@ -1,7 +1,7 @@
 from rest_framework import status, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework_simplejwt.tokens import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenObtainPairView
 from .serializers import (
     UserSerializer,
     RegisterSerializer,
