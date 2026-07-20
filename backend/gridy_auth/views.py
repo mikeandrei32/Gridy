7d90631c811d8f10dcf3a7f4b545398597f3a802
ur1el0 <zanoroosc@gmail.com>
2026-07-20 12:46:35 +0800

    feat(auth): added register and customtokenpair view endpoints

--- backend/gridy_auth/views.py
+++ backend/gridy_auth/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import status, permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import TokenObtainPairView
+from .serializers import (
+    UserSerializer,
+    RegisterSerializer,
+    CustomTokenObtainPairSerializer,
+)
 
 # Create your views here.
+
+class RegisterView(APIView):
+    permission_classes = [permissions.AllowAny]
+    
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response(
+                UserSerializer(user).data,
+                status=status.HTTP_201_CREATED
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+    
