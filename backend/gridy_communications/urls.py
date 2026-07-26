63a6368a38ea419ce645c2a3856f4df8d856e074
ur1el0 <zanoroosc@gmail.com>
2026-07-26 17:31:47 +0800

    fix(communications): resolve circular import and clean up misspelled serializer file

--- backend/gridy_communications/urls.py
+++ backend/gridy_communications/urls.py
@@ -1,4 +1,3 @@
-from backend.config.urls import urlpatterns
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from gridy_communications.views import AnnouncementViewSet, ActivityScheduleViewSet
