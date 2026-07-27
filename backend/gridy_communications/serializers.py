de04f24edc62ca053dab4dc1e660fafd7d92c31a
ur1el0 <zanoroosc@gmail.com>
2026-07-27 23:52:16 +0800

    feat(communications): implement FCMDeviceSerializer

--- backend/gridy_communications/serializers.py
+++ backend/gridy_communications/serializers.py
@@ -1,5 +1,11 @@
 from rest_framework import serializers
-from gridy_communications.models import Announcement, ActivitySchedule
+from gridy_communications.models import Announcement, ActivitySchedule, FCMDevice
+
+class FCMDeviceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FCMDevice
+        fields = ['id', 'token', 'created_at']
+
 
 class AnnouncementSerializer(serializers.ModelSerializer):
     class Meta:
