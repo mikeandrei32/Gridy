f93cb9418b24664c3759a42dcacd802342bd5442
ur1el0 <zanoroosc@gmail.com>
2026-07-27 23:52:58 +0800

    feat(communications): register devices route in router

--- backend/gridy_communications/urls.py
+++ backend/gridy_communications/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from gridy_communications.views import AnnouncementViewSet, ActivityScheduleViewSet
+from gridy_communications.views import AnnouncementViewSet, ActivityScheduleViewSet, FCMDeviceViewSet
 
 router = DefaultRouter()
 router.register(r'announcements', AnnouncementViewSet, basename='announcement')
 router.register(r'activities', ActivityScheduleViewSet, basename='activity')
+router.register(r'devices', FCMDeviceViewSet, basename='fcm-device')
+
 
 urlpatterns = [
     path('', include(router.urls)),
