253b3f63677da2fd2d011b26f2ad2678973c7657
ur1el0 <zanoroosc@gmail.com>
2026-07-28 23:55:28 +0800

    fix(auth): register UserProfileView endpoint under /me/

--- backend/gridy_auth/urls.py
+++ backend/gridy_auth/urls.py
@@ -1,3 +1,4 @@
+from gridy_auth.views import UserProfileView
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 from .views import RegisterView, CustomTokenObtainPairView
@@ -6,4 +7,5 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='auth_register'),
     path('login/', CustomTokenObtainPairView.as_view(), name='auth_login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='auth_token_refresh'),
+    path('me/', UserProfileView.as_view(), name='auth_me'),
 ]
