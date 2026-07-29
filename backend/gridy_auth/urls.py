52d9b9f8083a221a8f0c3a13b13137331d22b58e
ur1el0 <zanoroosc@gmail.com>
2026-07-29 12:18:14 +0800

    feat(auth): register resident import route under /import-residents/

--- backend/gridy_auth/urls.py
+++ backend/gridy_auth/urls.py
@@ -1,11 +1,12 @@
 from gridy_auth.views import UserProfileView
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import RegisterView, CustomTokenObtainPairView
+from .views import RegisterView, CustomTokenObtainPairView, ResidentImportView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='auth_register'),
     path('login/', CustomTokenObtainPairView.as_view(), name='auth_login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='auth_token_refresh'),
     path('me/', UserProfileView.as_view(), name='auth_me'),
+    path('import-residents/', ResidentImportView.as_view(), name='import_residents'),
 ]
